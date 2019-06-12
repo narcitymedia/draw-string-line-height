3bb8de75352b8fa54cc2fd72b73c1ab7adc01e56
Gabriel Cardinal <gabriel.cardinal@narcitymedia.com>
2019-06-12 17:24:17 -0400

    Extension method to compute the text wrap for a given text, font and width

--- GraphicsExtension.cs
+++ GraphicsExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
@@ -18,33 +19,58 @@ namespace draw_string_line_height
 
         // }
 
-        public static SizeF MeasureString(this Graphics that, string text, Font font, int width, int lineHeight)
+        /// <summary>
+        /// Calculates how the given text will have to be wrapped nin order to fit in the given width
+        /// </summary>
+        /// <param name="text">The full text for which to calculate the text wrap</param>
+        /// <param name="font">he font tht will be used to display the text</param>
+        /// <param name="maxWidth">The width in which the text must fit</param>
+        /// <returns>
+        /// A string enumerable where each string is a substring of the text param and represents a line
+        /// resulting of the text wrapping
+        /// </returns>
+        public static IEnumerable<string> GetLinesWrap(this Graphics that, string text, Font font, int maxWidth)
+        {
+            // See https://stackoverflow.com/questions/6111298/best-way-to-specify-whitespace-in-a-string-split-operation
+            string[] words = text.Split((char[]) null, StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length == 0)
+                return new string[0];
+
+            List<List<string>> lines = new List<List<string>>();
+            List<string> l = new List<string>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                SizeF subStrSize = that.MeasureString(words[i], font);
+                if (subStrSize.Width < maxWidth)
+                {
+                    l.Add(words[i]);
+                }
+                else
+                {
+                    // Add the current line to the lines list
+                    lines.Add(new List<string>(l));
+
+                    // Clear the lit used to store the words of the current line
+                    l.Clear();
+
+                    // Since the current word did not fit in the current line, add it to the new one
+                    l.Add(words[i]);
+                }
+            }
+
+            return new string[0];
+        }
+
+        public static SizeF MeasureString(this Graphics that, string text, Font font, int maxWidth, int lineHeight)
         {
             if (text == null || text.Length == 0)
                 return new SizeF(0, 0); 
             if (font == null)
                 throw new ArgumentNullException("font");
 
-            // See https://stackoverflow.com/questions/6111298/best-way-to-specify-whitespace-in-a-string-split-operation
-            string[] words = text.Split((char[]) null, StringSplitOptions.RemoveEmptyEntries);
-
-            if (words.Length == 0)
-                return new SizeF(0, 0);
-
-            List<List<string>> lines = new List<List<string>>();
-            
-            // This list is used to store each new line's words and cleared when text needs to wrap,
-            // hopefully reducing memory allocation
-            List<string> lineWords = new List<string>();
-
-            for (int i = 0; i < words.Length; i++)
-            {
-                SizeF subStrSize = that.MeasureString(words[i], font);
-                if (subStrSize.Width > width)
-                {
-                    
-                }
-            }
+            string[] lines = that.GetLinesWrap(text, font, maxWidth).ToArray();
 
             return new SizeF(1,1);
         }
