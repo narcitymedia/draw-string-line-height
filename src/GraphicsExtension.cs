a02eadaea282eddd3b8f2acfbe060d5fa3dc3773
Gabriel Cardinal <gabrielc@outlook.fr>
2019-06-13 10:26:43 -0400

    Moved project to src subfolder

--- src/GraphicsExtension.cs
+++ src/GraphicsExtension.cs
@@ -23,7 +23,7 @@ namespace draw_string_line_height
         /// A string enumerable where each string is a substring of the text param and represents a line
         /// resulting of the text wrapping
         /// </returns>
-        public static IEnumerable<string> GetLinesWrap(this Graphics that, string text, Font font, int maxWidth)
+        public static IEnumerable<string> GetWrappedLines(this Graphics that, string text, Font font, int maxWidth)
         {
             // See https://stackoverflow.com/questions/6111298/best-way-to-specify-whitespace-in-a-string-split-operation
             string[] words = text.Split((char[]) null, StringSplitOptions.RemoveEmptyEntries);
@@ -31,7 +31,7 @@ namespace draw_string_line_height
             if (words.Length == 0)
                 return new string[0];
 
-            List<List<string>> lines = new List<List<string>>();
+            List<string> lines = new List<string>();
             List<string> l = new List<string>();
 
             for (int i = 0; i < words.Length; i++)
@@ -44,7 +44,7 @@ namespace draw_string_line_height
                 else
                 {
                     // Add the current line to the lines list
-                    lines.Add(new List<string>(l));
+                    lines.Add(String.Join(' ', l));
 
                     // Clear the list used to store the words of the current line
                     l.Clear();
@@ -54,7 +54,7 @@ namespace draw_string_line_height
                 }
             }
 
-            return new string[0];
+            return lines;
         }
 
         /// <summary>
@@ -73,7 +73,9 @@ namespace draw_string_line_height
             if (font == null)
                 throw new ArgumentNullException("font");
 
-            string[] lines = that.GetLinesWrap(text, font, maxWidth).ToArray();
+            string[] lines = that.GetWrappedLines(text, font, maxWidth).ToArray();
+            foreach (string l in lines)
+                Console.WriteLine(l);
 
             return new SizeF(1,1);
         }
