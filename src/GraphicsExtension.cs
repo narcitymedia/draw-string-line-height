f2a20ecf265d12e0682c1e8d20b1c090a8db9c69
Gabriel Cardinal <gabriel.cardinal@narcitymedia.com>
2019-06-13 20:16:51 -0400

    GetWraeppedLines + MeasureString Graphics extension methods

--- src/GraphicsExtension.cs
+++ src/GraphicsExtension.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 
 namespace draw_string_line_height
 {
@@ -18,39 +15,39 @@ namespace draw_string_line_height
         /// </summary>
         /// <param name="text">The full text for which to calculate the text wrap</param>
         /// <param name="font">The font tht will be used to display the text</param>
-        /// <param name="maxWidth">The width in which the text must fit</param>
+        /// <param name="maxWidth">A positive number (grater than zero) that represents the width in which the text must fit</param>
         /// <returns>
         /// A string enumerable where each string is a substring of the text param and represents a line
         /// resulting of the text wrapping
         /// </returns>
-        public static IEnumerable<string> GetWrappedLines(this Graphics that, string text, Font font, int maxWidth)
+        public static IEnumerable<string> GetWrappedLines(this Graphics that, string text, Font font, double maxWidth = double.PositiveInfinity)
         {
+            if (String.IsNullOrEmpty(text)) return new string[0];
+            if (font == null) throw new ArgumentNullException("font", "The 'font' parameter must not be null");
+            if (maxWidth <= 0) throw new ArgumentOutOfRangeException("maxWidth", "Maximum width must be greater than zero");
+
             // See https://stackoverflow.com/questions/6111298/best-way-to-specify-whitespace-in-a-string-split-operation
             string[] words = text.Split((char[]) null, StringSplitOptions.RemoveEmptyEntries);
 
-            if (words.Length == 0)
-                return new string[0];
+            if (words.Length == 0) return new string[0];
 
             List<string> lines = new List<string>();
-            List<string> l = new List<string>();
+            string currentLine = String.Empty;
 
-            for (int i = 0; i < words.Length; i++)
+            for (int i = 0; i < words.Length; i++)
             {
-                SizeF subStrSize = that.MeasureString(words[i], font);
-                if (subStrSize.Width < maxWidth)
+                string word = words[i];
+                string potentialLine = currentLine + " " + word;
+                float potentialWidth = that.MeasureString(potentialLine, font).Width;
+                if (potentialWidth > maxWidth)
                 {
-                    l.Add(words[i]);
+                    lines.Add(currentLine);
+                    currentLine = word;
                 }
                 else
                 {
-                    // Add the current line to the lines list
-                    lines.Add(String.Join(' ', l));
-
-                    // Clear the list used to store the words of the current line
-                    l.Clear();
-
-                    // Since the current word did not fit in the current line, add it to the new one
-                    l.Add(words[i]);
+                    currentLine = potentialLine;
+                    if (i + 1 == words.Length) lines.Add(currentLine);
                 }
             }
 
@@ -63,7 +60,7 @@ namespace draw_string_line_height
         /// <param name="that">The extended <see cref="System.Drawing.Graphics" /> object</param>
         /// <param name="text">The text to measure</param>
         /// <param name="font">The font used to display the text</param>
-        /// <param name="maxWidth">The width in which the text must fit</param>
+        /// <param name="maxWidth">A positive number (grater than zero) that represents the width in which the text must fit</param>        
         /// <param name="lineHeight">The custom line height used to calculate the text size</param>
         /// <returns>The size taken up by the given text with the given parameters</returns>
         public static SizeF MeasureString(this Graphics that, string text, Font font, int maxWidth, int lineHeight)
@@ -74,10 +71,26 @@ namespace draw_string_line_height
                 throw new ArgumentNullException("font");
 
             string[] lines = that.GetWrappedLines(text, font, maxWidth).ToArray();
-            foreach (string l in lines)
-                Console.WriteLine(l);
+            
+            if (lines.Length == 0) return new SizeF(0, 0);
 
-            return new SizeF(1,1);
+            return new SizeF(maxWidth, lineHeight * lines.Length);
+        }
+
+
+        public static Region[] DrawString(this Graphics that, string text, Font font, Brush brush, int maxWidth,
+                                            int lineHeight, RectangleF layoutRectangle, StringFormat format)
+        {
+            string[] lines = that.GetWrappedLines(text, font, maxWidth).ToArray();
+            Region[] regions = new Region[lines.Length];
+
+            foreach (string line in lines)
+            {
+                
+                that.DrawString(line, font, brush,layoutRectangle);
+            }
+
+            return regions;
         }
     }
 }
